17480e93521626e651c711eb3493a05c947f4555
mohammed3120 <mohammedabduallah95@gmail.com>
2022-05-23 13:02:18 +0300

    two ways to calculate text warp

--- text_warper.py
+++ text_warper.py
@@ -1,12 +1,19 @@
-from re import sub
 def wrap(string, max_width):
     result = ''
     for i in range(0, len(string),max_width):
         result += string[i:i+max_width]+'\n'
     print(result)
+import textwrap
+def text_wrap(string, max_width):
+    wrapper = textwrap.TextWrapper(width= max_width)
+  
+    word_list = wrapper.wrap(text = string)
+    for line in word_list:
+        print(line)
 
 
 
 
 if __name__=="__main__":
     wrap('ABCDEFGHIJKLMNOPRS', 4)
+    text_wrap('ABCDEFGHIJKLMNOPRS', 4)
